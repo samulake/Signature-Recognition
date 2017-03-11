0dfa456bb5cf4b6d54067b66ef855311eb4d12dd
Eryk <Eryk@192.168.1.15>
2017-03-11 22:59:48 +0100

    implement processImageTest() and reduceNoiseTest()

--- SignatureRecognitionApplication/test/test/OpenCVSignatureImageProcessorImplemenorTest.java
+++ SignatureRecognitionApplication/test/test/OpenCVSignatureImageProcessorImplemenorTest.java
@@ -17,7 +17,7 @@ import org.opencv.imgcodecs.Imgcodecs;
 import preprocessing.OpenCVSignatureImageProcessorImplemenor;
 
 public class OpenCVSignatureImageProcessorImplemenorTest {
-	private OpenCVSignatureImageProcessorImplemenor testedClass;
+	private OpenCVSignatureImageProcessorImplemenor testedClass = new OpenCVSignatureImageProcessorImplemenor();
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -30,7 +30,6 @@ public class OpenCVSignatureImageProcessorImplemenorTest {
 
 	@Before
 	public void setUp() throws Exception {
-		testedClass = new OpenCVSignatureImageProcessorImplemenor();
 	}
 
 	@After
@@ -47,17 +46,40 @@ public class OpenCVSignatureImageProcessorImplemenorTest {
 		fail("Not yet implemented");
 	}
 
-	//@Test
-	public void testProcessImage() {
-		fail("Not yet implemented");
+	@Test(timeout=1000)
+	public void testProcessImage() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		readImageTest();
+		reduceNoiseTest();
+		eliminateBackgroundTest();
 	}
 	
-	@Test
+	
 	public void readImageTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Method testedMethod = testedClass.getClass().getDeclaredMethod("readImage", String.class);
 		testedMethod.setAccessible(true);
-		testedMethod.invoke(testedClass, "./testData/signature.jpg");
+		testedMethod.invoke(testedClass, "./testData/image.jpg");
 		Mat image = (Mat) testedClass.getImage();
+		assertNotNull(image);
+		assertTrue(image.channels() == 1);
+		Imgcodecs.imwrite("./testData/grayImage.jpg", image);
+	}
+	
+	public void eliminateBackgroundTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Method testedMethod = testedClass.getClass().getDeclaredMethod("eliminateBackground", null);
+		testedMethod.setAccessible(true);
+		testedMethod.invoke(testedClass);
+		Mat image = (Mat) testedClass.getImage();
+		Imgcodecs.imwrite("./testData/eliminatedBackgroundImage.jpg", image);
+		assertNotNull(image);
+		assertTrue(image.channels() == 1);
+	}
+	
+	public void reduceNoiseTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Method testedMethod = testedClass.getClass().getDeclaredMethod("reduceNoise", null);
+		testedMethod.setAccessible(true);
+		testedMethod.invoke(testedClass);
+		Mat image = (Mat) testedClass.getImage();
+		Imgcodecs.imwrite("./testData/reducedNoiseImage.jpg", image);
 		assertNotNull(image);
 		assertTrue(image.channels() == 1);
 	}
