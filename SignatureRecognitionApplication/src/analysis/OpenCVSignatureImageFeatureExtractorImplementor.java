e82f7da5e449e12b259906c8cd443e39585ddfd2
Eryk <Eryk@192.168.0.206>
2017-04-06 11:08:03 +0200

    Previous revision

--- SignatureRecognitionApplication/src/analysis/OpenCVSignatureImageFeatureExtractorImplementor.java
+++ SignatureRecognitionApplication/src/analysis/OpenCVSignatureImageFeatureExtractorImplementor.java
@@ -1,5 +1,10 @@
 package analysis;
 
+/**
+ * @author Eryk
+ * @version 1.0
+ * @created 06-mar-2017 18:20:28
+ */
 public class OpenCVSignatureImageFeatureExtractorImplementor extends SignatureImageFeatureExtractorImplementor {
 
 	public OpenCVSignatureImageFeatureExtractorImplementor(){
