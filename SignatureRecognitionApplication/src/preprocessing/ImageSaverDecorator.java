f686b25936f1030c586e21e1edd1a2c7f82e40c3
Eryk <Eryk@192.168.1.4>
2017-04-18 15:07:51 +0200

    ImageSaverDecorator only saves input and output image for processImage method

--- SignatureRecognitionApplication/src/preprocessing/ImageSaverDecorator.java
+++ SignatureRecognitionApplication/src/preprocessing/ImageSaverDecorator.java
@@ -5,7 +5,6 @@ import org.opencv.imgcodecs.Imgcodecs;
 
 public class ImageSaverDecorator extends SignatureImageProcessorDecorator {
 	private final String imagesFolderPath = "./data/";
-	private final String testResultsFolderPath = "./testData/";
 	
 	public ImageSaverDecorator(SignatureImageProcessor component) {
 		super(component);
@@ -26,30 +25,25 @@ public class ImageSaverDecorator extends SignatureImageProcessorDecorator {
 	@Override
 	public void readImage(String sourcePath) {
 		super.readImage(sourcePath);
-		Imgcodecs.imwrite(testResultsFolderPath + "grayScale.jpg", (Mat)super.getImage());
 	}
 
 	@Override
 	public void eliminateBackground() {
 		super.eliminateBackground();
-		Imgcodecs.imwrite(testResultsFolderPath + "eliminatedBackground.jpg", (Mat)super.getImage());
 	}
 
 	@Override
 	public void reduceNoise() {
 		super.reduceNoise();
-		Imgcodecs.imwrite(testResultsFolderPath + "reducedNoise.jpg", (Mat)super.getImage());
 	}
 
 	@Override
 	public void normalizeWidth(int width) {
 		super.normalizeWidth(width);
-		Imgcodecs.imwrite(testResultsFolderPath + "nozmalizedSize.jpg", (Mat)super.getImage());
 	}
 
 	@Override
 	public void thin() {
 		super.thin();
-		Imgcodecs.imwrite(testResultsFolderPath + "thinnedImage.jpg", (Mat)super.getImage());
 	}
 }
