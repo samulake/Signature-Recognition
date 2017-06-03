1c06327134868bd1280dfebbb226f9e6777f51b6
szalankk <szalankk@ee.iem.pw.edu.pl>
2017-06-03 14:38:43 +0200

    dodanie funkcji znajdującej najniżej i najwyżej położony czarny piksel

--- SignatureRecognitionApplication/src/analysis/SignatureImageFeatureExtractorImplementor.java
+++ SignatureRecognitionApplication/src/analysis/SignatureImageFeatureExtractorImplementor.java
@@ -1,5 +1,8 @@
 package analysis;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+
 public class SignatureImageFeatureExtractorImplementor implements SignatureImageFeatureExtractor {
 
 	public SignatureImageFeatureExtractorImplementor(){
@@ -25,5 +28,43 @@ public class SignatureImageFeatureExtractorImplementor implements SignatureImage
 	public Object extractMaskFeatures(){
 		return null;
 	}
+	
+	public Point getHighestBlackPixel(Mat image) {
+		double [] pixel = null; 
+		int i, j = 0;
+		Point p = null;
+		
+		for (i = 0; i < image.height(); i++) {
+			for (j = 0; j < image.width(); j++){
+				pixel = image.get(i, j);
+				if (pixel[0] == 0.0f) {
+					p = new Point(j, i);
+					return p;
+				}
+			}
+		}
+		
+		return p;
+	}
+	
+	
+	public Point getLowestBlackPixel(Mat image) {
+		double [] pixel = null; 
+		int i, j = 0;
+		Point p = null;
+		
+		for (i = image.height() - 1; i >= 0 ; i--) {
+			for (j = 0; j < image.width(); j++){
+				pixel = image.get(i, j);
+				if (pixel[0] == 0.0f) {
+					p = new Point(j, i);
+					return p;
+				}
+			}
+		}
+		
+		return p;
+	}
+	
 
 }
