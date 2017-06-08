e35c4e3dee480fd60f5f7606599836cb17be99e9
szalankk <szalankk@ee.iem.pw.edu.pl>
2017-06-08 20:29:07 +0200

    dodanie funkcji obliczającej stosunek wysokości podpisu do jego szerokości

--- SignatureRecognitionApplication/src/analysis/SignatureImageFeatureExtractorImplementor.java
+++ SignatureRecognitionApplication/src/analysis/SignatureImageFeatureExtractorImplementor.java
@@ -1,5 +1,8 @@
 package analysis;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+
 public class SignatureImageFeatureExtractorImplementor implements SignatureImageFeatureExtractor {
 
 	public SignatureImageFeatureExtractorImplementor(){
@@ -26,4 +29,87 @@ public class SignatureImageFeatureExtractorImplementor implements SignatureImage
 		return null;
 	}
 
+	
+	public Point getHighestBlackPixel(Mat image) {
+		double [] pixel = null; 
+		int i, j = 0;
+		Point p = null;
+		
+		for (i = 0; i < image.height(); i++) {
+			for (j = 0; j < image.width(); j++){
+				pixel = image.get(i, j);
+				if (pixel[0] == 0.0f) {
+					p = new Point(j, i);
+					return p;
+				}
+			}
+		}
+		
+		return p;
+	}
+	
+	
+	public Point getLowestBlackPixel(Mat image) {
+		double [] pixel = null; 
+		int i, j = 0;
+		Point p = null;
+		
+		for (i = image.height() - 1; i >= 0 ; i--) {
+			for (j = 0; j < image.width(); j++){
+				pixel = image.get(i, j);
+				if (pixel[0] == 0.0f) {
+					p = new Point(j, i);
+					return p;
+				}
+			}
+		}
+		
+		return p;
+	}
+	
+	public Point getLeftestBlackPixel(Mat image) {
+		double [] pixel = null; 
+		int i, j = 0;
+		Point p = null;
+		
+		for (i = 0; i < image.width(); i++) {
+			for (j = 0; j < image.height(); j++){
+				pixel = image.get(j, i);
+				if (pixel[0] == 0.0f) {
+					p = new Point(i, j);
+					return p;
+				}
+			}
+		}
+		
+		return p;
+	}
+	
+	public Point getRightestBlackPixel(Mat image) {
+		double [] pixel = null; 
+		int i, j = 0;
+		Point p = null;
+		
+		for (i = image.width() - 1; i >= 0 ; i--) {
+			for (j = 0; j < image.height(); j++){
+				pixel = image.get(j, i);
+				if (pixel[0] == 0.0f) {
+					p = new Point(i, j);
+					return p;
+				}
+			}
+		}
+		
+		return p;
+	}
+	
+	
+	public float getHeightWidthRatio(Mat image) {
+		return (float) (getLowestBlackPixel(image).y - getHighestBlackPixel(image).y) / (float) (getRightestBlackPixel(image).x - getLeftestBlackPixel(image).x);
+	}
+	
+	
+	
+	
+	
 }
