2ca9836b7a34427a78837c2702941bd50938cd31
Eryk <Eryk@192.168.0.206>
2017-06-09 20:49:41 +0200

    Add classifiers names to ClassifierFactory interface

--- SignatureRecognitionApplication/src/classification/ClassifierFactory.java
+++ SignatureRecognitionApplication/src/classification/ClassifierFactory.java
@@ -4,6 +4,10 @@ import weka.classifiers.Classifier;
 import weka.core.Instances;
 
 public interface ClassifierFactory {
+	String DECISION_TREE = "Decision tree";
+	String NEURAL_NETWORK = "Neural network";
+	String NAIVE_BAYES_CLASSIFIER = "Naive Bayes classifier";
+	
 	public Classifier buildDecisionTree(Instances trainData);
 	
 	public Classifier buildNeuralNetwork(Instances trainData);
